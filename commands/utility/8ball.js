91f40dd4c8c82d2d756d388d9dbe8161387bcef2
devilsamop@gmail.com <devilsamop@gmail.com>
2025-06-06 20:30:04 +0530

    8 Ball Commands

--- commands/utility/8ball.js
+++ commands/utility/8ball.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder} = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,15 +8,18 @@ module.exports = {
             option.setName('question')
                 .setDescription('The question you want to ask the magic 8-ball.')
                 .setRequired(true)),
+                
     async execute(interaction) {
         const question = interaction.options.getString('question'); 
         const answers = ['yes', 'no', 'maybe', 'definitely', 'ask again later', 'absolutely not', 'certainly', 'probably not'];
         const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-        const embed = new Discord.MessageEmbed()
+        const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('Magic 8-Ball')
-            .addField('Question', question)
-            .addField('Answer', randomAnswer)
+            .addFields(
+                { name: 'Question', value: question },
+                { name: 'Answer', value: randomAnswer }
+            )
             .setTimestamp()
             .setFooter({ text: 'Ask your question!' });
         await interaction.reply({ embeds: [embed] });
