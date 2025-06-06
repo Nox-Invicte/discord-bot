0ecc2d563bdcb20199c80e3e3cf9426672c7d38b
devilsamop@gmail.com <devilsamop@gmail.com>
2025-06-06 19:49:42 +0530

    Commands Handling and Event Handling

---
+++ commands/utility/intro.js
@@ -0,0 +1,11 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('intro')
+        .setDescription('Provides a brief introduction to the bot and its features.'),
+    async execute(interaction) {
+        await interaction.reply(`Hello! I am ${interaction.client.user.username}, a bot designed by @nox_invicte`);
+    }
+};
+// This command provides a brief introduction to the bot and its features.
