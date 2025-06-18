4cbc46c55e02e6c270b97740681a941b8e9b0fc9
devilsamop@gmail.com <devilsamop@gmail.com>
2025-06-18 20:30:46 +0530

    Fixed Issues

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,12 +1,26 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
+		const server = interaction.guild;
+		const embed = new EmbedBuilder()
+		.setColor('#0099ff')
+		.setTitle('Server Information')
+		.addFields(
+			{ name: 'Server Name', value: server.name, inline: true },
+			{ name: 'Member Count', value: server.memberCount.toString(), inline: true },
+			{ name: 'Created At', value: server.createdAt.toDateString(), inline: true },
+			{ name: 'Owner', value: `<@${server.ownerId}>`, inline: true }
+		)
+		.setThumbnail(server.iconURL() || '')
+		.setTimestamp()
+		await interaction.reply({ embeds: [embed] });
+
 		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		
 	},
 };
 // This command provides basic information about the server where the command was invoked.
